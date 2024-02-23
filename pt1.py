4c72b721e2ef9d73e41ba5d7615cb7412f7cd5f5
Alex Jimenez <145340934+alexxJimenez@users.noreply.github.com>
2024-02-23 12:54:56 +0100

    Update pt1.py

--- pt1.py
+++ pt1.py
@@ -1,11 +1,19 @@
 import random
-
-
-
+frase_separada = []
 def obtener_frase():
-    global frase
     frase = input().split()
-    return frase
+    global frase_separada
+    frase_separada = frase
+    return frase_separada
 
-frase_separada = obtener_frase(frase)
-print(frase_separada)
+def tratar_palabras():
+    for palabra in frase_separada:
+        for letra in palabra:
+            if letra == palabra[0] or letra == palabra[-1]:
+                frase_separada.append('')
+            else:
+                frase_separada.append(letra)
+    return frase_separada
+obtener_frase()
+tratar_palabras()
+print(tratar_palabras())
