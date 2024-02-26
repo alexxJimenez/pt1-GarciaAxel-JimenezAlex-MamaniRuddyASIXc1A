f902b7db39db5e7b03269a5def1d00c3d2a8719c
alexxJimenez <alex.jimenez.7e7@itb.cat>
2024-02-26 10:31:20 +0100

    aa

--- pruebaalex.py
+++ pruebaalex.py
@@ -1,25 +1,27 @@
 import random
+
+
 def desordenar_lletres(paraula):
-    if len(paraula) <= 2:
-        return paraula
-    else:
-        lletres_interiors = list(paraula[1:-1])
-        random.shuffle(lletres_interiors)
-        return paraula[0] + ''.join(lletres_interiors) + paraula[-1]
+    lletres_interiors = list(paraula[1:-1])
+    random.shuffle(lletres_interiors)
+    return paraula[0] + ''.join(lletres_interiors) + paraula[-1]
+
 
 def frase(text):
     paraules = text.split()
     text_desordenat = []
     for paraula in paraules:
-        if len(paraula) > 2:
+        if len(paraula) > 3:
             text_desordenat.append(desordenar_lletres(paraula))
         else:
             text_desordenat.append(paraula)
     return ' '.join(text_desordenat)
 
+
 def imprimir_text():
     usuari = input("Introdueix el text: ")
     print("Text desordenat: ")
     print(frase(usuari))
+
 
 imprimir_text()
