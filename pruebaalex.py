beac41ec2175f9ba8e40664d5afbc0023374be62
alexxJimenez <alex.jimenez.7e7@itb.cat>
2024-02-26 12:01:29 +0100

    aa

--- pruebaalex.py
+++ pruebaalex.py
@@ -1,11 +1,26 @@
+"""
+Nom: Axel Garcia Hernandez, Alex Jimenez Navarro, Ruddy Alan Mamani Quiñones
+ASIXc 1A M03 UF1
+Data: 26/02/2024
+Descripció: Programa que desordena les lletres interiors de les paraules d'un text
+"""
 import random
 
 
 def desordenar_lletres(paraula):
-    lletres_interiors = list(paraula[1:-1])
-    random.shuffle(lletres_interiors)
-    return paraula[0] + ''.join(lletres_interiors) + paraula[-1]
-
+    if paraula.isalnum():
+        lletres_interiors = list(paraula[1:-1])
+        random.shuffle(lletres_interiors)
+        return paraula[0] + ''.join(lletres_interiors) + paraula[-1]
+    else:
+        if paraula[0].isalnum():
+            lletres_interiors = list(paraula[1:-2])
+            random.shuffle(lletres_interiors)
+            return paraula[0] + ''.join(lletres_interiors) + paraula[-2] + paraula[-1]
+        else:
+            lletres_interiors = list(paraula[2:-2])
+            random.shuffle(lletres_interiors)
+            return paraula[0] + paraula[1] + ''.join(lletres_interiors) + paraula[-2] + paraula[-1]
 
 def frase(text):
     paraules = text.split()
@@ -20,8 +35,7 @@ def frase(text):
 
 def imprimir_text():
     usuari = input("Introdueix el text: ")
-    print("Text desordenat: ")
-    print(frase(usuari))
+    print("Text desordenat:", frase(usuari))
 
 
 # main
