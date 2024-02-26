691c2713f160552fb3b261728ac206a9dd4c5708
ruddy <ruddy.mamani.7e5@itb.cat>
2024-02-26 12:12:22 +0100

    demo

--- Demo random.py
+++ Demo random.py
@@ -1,78 +1,39 @@
-"""
 import random
 
-def desordenar_palabra(palabra):
-    # Convertir la palabra en una lista de caracteres
-    lista_letras = list(palabra)
-
-    # Mantener la primera y última letra en su lugar
-    primera_letra = lista_letras[0]
-    ultima_letra = lista_letras[-1]
-
-    # Desordenar las letras intermedias
-    letras_intermedias = lista_letras[1:-1]
-    random.shuffle(letras_intermedias)
-
-    # Crear la palabra desordenada
-    palabra_desordenada = primera_letra + ''.join(letras_intermedias) + ultima_letra
-
-    return palabra_desordenada
-
-# Pedir al usuario una palabra
-palabra_original = input("Introduce una palabra: ")
-
-# Desordenar la palabra y mostrar el resultado
-palabra_desordenada = desordenar_palabra(palabra_original)
-print("Palabra desordenada:", palabra_desordenada)
-"""
-
-import random
-
-def desordenar_letras(palabra):
-    # Convertir la palabra en una lista de caracteres
-    lista_letras = list(palabra)
-
-    # Mantener la primera y última letra en su lugar
-    primera_letra = lista_letras[0]
-    ultima_letra = lista_letras[-1]
-
-    # Desordenar las letras intermedias
-    letras_intermedias = lista_letras[1:-1]
-    random.shuffle(letras_intermedias)
-
-    # Crear la palabra desordenada
-    palabra_desordenada = primera_letra + ''.join(letras_intermedias) + ultima_letra
-
-    return palabra_desordenada
-
-def procesar_palabra(palabra):
-    # Ignorar palabras con menos de tres letras
-    if len(palabra) < 3:
-        return palabra
+def mantenir_arroba(paraula):
+    if "@" in paraula:
+        return paraula
     else:
-        return desordenar_letras(palabra)
+        return desordenar_lletres(paraula)
 
-def procesar_frase(frase):
-    # Dividir la frase en palabras
-    palabras = frase.split()
+def desordenar_lletres(paraula):
+    if paraula.isalnum():
+        lletres_interiors = list(paraula[1:-1])
+        random.shuffle(lletres_interiors)
+        return paraula[0] + ''.join(lletres_interiors) + paraula[-1]
+    else:
+        if paraula[0].isalnum():
+            lletres_interiors = list(paraula[1:-2])
+            random.shuffle(lletres_interiors)
+            return paraula[0] + ''.join(lletres_interiors) + paraula[-2] + paraula[-1]
+        else:
+            lletres_interiors = list(paraula[2:-2])
+            random.shuffle(lletres_interiors)
+            return paraula[0] + paraula[1] + ''.join(lletres_interiors) + paraula[-2] + paraula[-1]
 
-    # Procesar cada palabra
-    palabras_procesadas = [procesar_palabra(palabra) for palabra in palabras]
+def frase(text):
+    paraules = text.split()
+    text_desordenat = []
+    for paraula in paraules:
+        if len(paraula) > 3:
+            text_desordenat.append(mantenir_arroba(paraula))
+        else:
+            text_desordenat.append(paraula)
+    return ' '.join(text_desordenat)
 
-    # Unir las palabras procesadas en una frase
-    frase_procesada = ' '.join(palabras_procesadas)
+def imprimir_text():
+    usuari = input("Introdueix el text: ")
+    print("Text desordenat:", frase(usuari))
 
-    return frase_procesada
-
-def obtener_frase_desordenada():
-    # Pedir al usuario una frase
-    frase_original = input("Introduce una frase: ")
-
-    # Procesar y desordenar la frase
-    frase_desordenada = procesar_frase(frase_original)
-
-    return frase_desordenada
-
-# Obtener y mostrar la frase desordenada
-frase_resultante = obtener_frase_desordenada()
-print("Frase desordenada:", frase_resultante)
+# main
+imprimir_text()
