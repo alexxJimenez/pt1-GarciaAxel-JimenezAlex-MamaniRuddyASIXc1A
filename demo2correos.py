353d854bdc4ceee01eec47e9016c22b8fa52abf2
alexxJimenez <alex.jimenez.7e7@itb.cat>
2024-02-26 13:03:31 +0100

    aa

--- demo2correos.py
+++ demo2correos.py
@@ -1,21 +1,14 @@
 import random
 
-def mantenir_arroba(paraula):
-    if "@" in paraula:
-        correu = paraula.split("@")
-        punt = correu.split(".")
-        for paraula in punt:
-            if len(paraula) > 3:
-                text_desordenat.append(mantenir_arroba(paraula))
-            else:
-                text_desordenat.append(paraula)
-        return ' '.join(text_desordenat)
-        return
-    else:
-        return desordenar_lletres(paraula)
-
 def desordenar_lletres(paraula):
-    if paraula.isalnum():
+    if "@" in paraula or "." in paraula:
+        at_index = paraula.index("@")
+        prefix = ''.join(random.sample(paraula[:at_index], len(paraula[:at_index])))
+        dot_index = paraula.index(".")
+        middle = ''.join(random.sample(paraula[at_index + 1:dot_index], len(paraula[at_index + 1:dot_index])))
+        suffix = paraula[dot_index:]
+        return prefix + paraula[at_index] + middle + suffix
+    elif paraula.isalnum():
         lletres_interiors = list(paraula[1:-1])
         random.shuffle(lletres_interiors)
         return paraula[0] + ''.join(lletres_interiors) + paraula[-1]
@@ -33,18 +26,10 @@ def frase(text):
     paraules = text.split()
     text_desordenat = []
     for paraula in paraules:
-        if "@" in paraula:
-            correu = paraula.split("@")
-            punt = correu.split(".")
-            if len(paraula) > 3:
-                text_desordenat.append(mantenir_arroba(paraula))
-            else:
-                text_desordenat.append(paraula)
+        if len(paraula) > 3:
+            text_desordenat.append(desordenar_lletres(paraula))
         else:
-            if len(paraula) > 3:
-                text_desordenat.append(mantenir_arroba(paraula))
-            else:
-                text_desordenat.append(paraula)
+            text_desordenat.append(paraula)
     return ' '.join(text_desordenat)
 
 def imprimir_text():
